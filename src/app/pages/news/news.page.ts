07deb34825054c4be6e2c04dec9aefa0e2b2562b
Paulo <pauloafpjunior@hotmail.com>
2019-09-13 18:53:44 -0300

    adicionando suporte a compartilhamento

--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsModel } from 'src/app/model/news.model';
 import { NewsService } from 'src/app/services/news.service';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-news',
@@ -10,10 +11,25 @@ import { NewsService } from 'src/app/services/news.service';
 export class NewsPage implements OnInit {
 
   private _lstNews: NewsModel[];
+  text = 'Check out the Ionic Academy!';
+  url = 'https://ionicacademy.com';
 
-  constructor(private newsService: NewsService) {
+  constructor(private newsService: NewsService,
+    private socialSharing: SocialSharing) {
     this._lstNews = this.newsService.getAll();
   }
+
+  async shareWhatsApp(id: number) {
+    let news: NewsModel = await this.newsService.searchById(id);
+
+    // Text + Image or URL works
+    this.socialSharing.shareViaWhatsApp(news.title, news.image, news.link).then(() => {
+      // Sucess
+    }).catch((e) => {
+      // Error!
+    });
+  }
+
 
   ngOnInit() {
   }
